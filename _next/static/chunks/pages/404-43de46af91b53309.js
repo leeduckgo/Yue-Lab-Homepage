019af9354cfd91f7e845bd07e063f563e9520fd2
leeduckgo <leeduckgo@gmail.com>
2025-01-30 21:03:19 +0800

    update with latest

---
+++ _next/static/chunks/pages/404-43de46af91b53309.js
@@ -0,0 +1,10 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[197],{394:function(t,e,r){(window.__NEXT_P=window.__NEXT_P||[]).push(["/404",function(){return r(7900)}])},7900:function(t,e,r){"use strict";r.r(e),r.d(e,{default:function(){return Error}});var s=r(5893);let n=`
+  body {
+    display: flex;
+  }
+  #__next {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+`;function Error(){return(0,s.jsxs)("div",{className:"flex flex-auto items-center justify-center text-center px-4 flex-col sm:flex-row",children:[(0,s.jsx)("h1",{className:"text-2xl sm:text-3xl font-extrabold text-slate-900 tracking-tight sm:pr-6 sm:mr-6 sm:border-r sm:border-slate-900/10 sm:dark:border-slate-300/10 dark:text-slate-200",children:"404"}),(0,s.jsx)("h2",{className:"mt-2 text-lg text-slate-700 dark:text-slate-400 sm:mt-0",children:"This page could not be found."}),(0,s.jsx)("style",{dangerouslySetInnerHTML:{__html:n}})]})}Error.layoutProps={meta:{title:"404"}}}},function(t){t.O(0,[774,888,179],function(){return t(t.s=394)}),_N_E=t.O()}]);
---
+++ _next/static/chunks/pages/examples/mobile-first-demo-50977f5d124207c3.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[510],{9155:function(n,_,e){(window.__NEXT_P=window.__NEXT_P||[]).push(["/examples/mobile-first-demo",function(){return e(327)}])}},function(n){n.O(0,[327,774,888,179],function(){return n(n.s=9155)}),_N_E=n.O()}]);
